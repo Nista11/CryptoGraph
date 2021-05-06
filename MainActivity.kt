6180cb2639766afe9bbaf7c689b6c31ad2e7200d
Goth11 <42772160+Goth11@users.noreply.github.com>
2021-05-06 23:29:33 +0300

    Add files via upload

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,5 +1,7 @@
 package com.goth.cryptograph
 
+import android.content.res.Resources
+import android.graphics.Color
 import android.os.Bundle
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
@@ -9,15 +11,21 @@ import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
-    private var cryptoData: List<Foo>? = null
+    private var cryptoData: List<Crypto>? = null
     private lateinit var tableLayout: TableLayout
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         getCryptoData()
-        createTable()
-        addTable()
+        if (cryptoData != null)
+        {
+            createTable()
+            addTable()
+        }
+        else
+            connectionErrorMessage()
     }
 
     private fun getCryptoData()
@@ -43,8 +51,21 @@ class MainActivity : AppCompatActivity() {
         {
             val tableRow = TableRow(this)
             val button = Button(this)
-            button.text = currentCrypto.name
-            button.width = 1050
+            if (currentCrypto.price_change_percentage_24h > 0)
+                button.setTextColor(Color.rgb(0, 200, 0))
+            else
+                button.setTextColor(Color.rgb(200, 0, 0))
+
+            var text = "${currentCrypto.name} (${currentCrypto.symbol}) ${currentCrypto.price} \$"
+            if (currentCrypto.price_change_percentage_24h > 0)
+                text += " +"
+            else
+                text += " "
+            text += "${currentCrypto.price_change_percentage_24h}%"
+            button.text = text
+
+            button.width = getScreenWidth()
+            button.height = getScreenHeight() / 6
             tableRow.addView(button)
             tableLayout.addView(tableRow)
         }
@@ -59,16 +80,33 @@ class MainActivity : AppCompatActivity() {
         setContentView(relativeLayout)
     }
 
+    private fun connectionErrorMessage()
+    {
+        val textView = TextView(this)
+        textView.text = "Unable to connect to services. Please check your connection and try again."
+        val relativeLayout: RelativeLayout = findViewById(R.id.relative_layout)
+        relativeLayout.addView(textView)
+        setContentView(relativeLayout)
+    }
+
+    fun getScreenWidth(): Int {
+        return Resources.getSystem().displayMetrics.widthPixels
+    }
+
+    fun getScreenHeight(): Int {
+        return Resources.getSystem().displayMetrics.heightPixels
+    }
+
     class Response(json: String) : JSONObject(json) {
         val data = this.optJSONArray("all_data")
             ?.let { 0.until(it.length()).map { i -> it.optJSONObject(i) } } // returns an array of JSONObject
-            ?.map { Foo(it.toString()) } // transforms each JSONObject of the array into Foo
+            ?.map { Crypto(it.toString()) } // transforms each JSONObject of the array into Crypto
     }
 
-    class Foo(json: String) : JSONObject(json) {
+    class Crypto(json: String) : JSONObject(json) {
         val name: String? = this.optString("name")
         val symbol: String? = this.optString("symbol")
-        val current_price = this.optDouble("current_price")
+        val price = this.optDouble("current_price")
         val price_change_percentage_24h = this.optDouble("price_change_percentage_24h")
         val last_updated = this.optString("last_updated")
         val sparkline_in_7d = this.optJSONObject("sparkline_in_7d")?.getJSONArray("price")
